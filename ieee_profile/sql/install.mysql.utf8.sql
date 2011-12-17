e477736ab2e80b79ecf6ef203fe257ea9e93ca5e
Thibaut Cuvelier <dourouc05@gmail.com>
2011-12-17 22:05:37 +0100

    Back to the ground

--- ieee_profile/sql/install.mysql.utf8.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `#__user_ieee_profiles`;
- 
-CREATE TABLE `#__user_ieee_profiles` (
-  `user_id` int(11) NOT NULL,
-  `profile_key` varchar(100) NOT NULL,
-  `profile_value` varchar(255) NOT NULL,
-  `ordering` int(11) NOT NULL DEFAULT '0',
-  UNIQUE KEY `idx_user_id_profile_key` (`user_id`,`profile_key`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='User IEEE profile storage table';
